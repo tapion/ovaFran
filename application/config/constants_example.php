42f3c40758624336f338d8b3cce88bb6b47b09b2
tapion <tapion@localhost>
2013-04-09 23:27:00 -0500

    Se agrega constante con el nombre del proyecto

--- application/config/constants_example.php
+++ application/config/constants_example.php
@@ -21,6 +21,7 @@ define('NOMBRE_BD_MOODLE', "moodle_tesis");
 define('ID_DOCENTE_MOODLE', 3);
 define('ID_ESTUDIANTE_MOODLE', 5);
 define('STRING_SEMILLA', '7BYc7B(svn2v`8{zOfH]Ji!h&');   
+define('PROJECT_NAME', 'ovaFran');   
 
 /*
 |--------------------------------------------------------------------------
