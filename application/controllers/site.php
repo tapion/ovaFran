8f1f1401244b855d183b6fe706fb3aaae63fed81
tapion <tapion@localhost>
2013-04-30 22:50:01 -0500

    Se agrega logueo desde moodle

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -6,14 +6,20 @@ if (!defined('BASEPATH'))
 class Site extends CI_Controller {
 
     function index() {
-        $this->load->library('session');
-        $userid = $this->session->userdata("userid");
-        if ($userid != "") {
+        if(empty($_GET["nom"]) || !is_numeric($_GET["nom"])){
+            exit('Parametros incorrectos');
+        }
+        $this->load->model("usuarios_model", "Usuarios", true);
+        $user = $this->Usuarios->login($_GET["nom"]);
+        $userData = $user->result();
+
+        if (isset($userData[0]->id)) {
             $this->load->view('header_view');
-            $this->load->view('site/index', array("role_id" => $this->session->userdata("role_id")));
+            $this->load->view('site/index', array("role_id" => $userData[0]->roleid));
+            $newUserData = array("userid" => $userData[0]->id, "username" => $userData[0]->username, "role_id" => $userData[0]->roleid);
+            $this->session->set_userdata($newUserData);
         } else {
-            $this->load->view('header_view');
-            $this->load->view('login/login');
+            exit('Usuario no existe');
         }
     }
 
@@ -23,26 +29,6 @@ class Site extends CI_Controller {
             $this->load->view($configItem["view"], $configItem["parameters"]);
         }
         $this->load->view('footer_view.php');
-    }
-
-    function login() {
-        $this->load->library('session');
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        $this->load->model("usuarios_model", "Usuarios", true);
-        $user = $this->Usuarios->login($username, $password);
-        $userData = $user->result();
-
-        if (isset($userData[0]->id)) {
-            $this->load->view('header_view');
-            $this->load->view('site/index', array("role_id" => $userData[0]->roleid));
-            $newUserData = array("userid" => $userData[0]->id, "username" => $userData[0]->username, "role_id" => $userData[0]->roleid);
-            $this->session->set_userdata($newUserData);
-        } else {
-            $this->load->view('header_view');
-            $this->load->view('login/login');
-        }
     }
 
     function logout() {
--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -1,7 +1,6 @@
 <?php
 class Usuarios_model extends CI_Model {
-    function login($username, $password){
-        $authentication = $this->db->query("SELECT u.id, u.username, u.firstname, u.lastname, u.email, ra.roleid FROM ".NOMBRE_BD_MOODLE.".mdl_user AS u INNER JOIN ".NOMBRE_BD_MOODLE.".mdl_role_assignments AS ra ON ra.userid = u.id WHERE u.username = '$username' AND u.password = MD5('".$password.STRING_SEMILLA."')");
-        return $authentication;
+    function login($idUsuarios){
+        return $this->db->query("SELECT u.id, u.username, u.firstname, u.lastname, u.email, ra.roleid FROM ".NOMBRE_BD_MOODLE.".mdl_user AS u INNER JOIN ".NOMBRE_BD_MOODLE.".mdl_role_assignments AS ra ON ra.userid = u.id WHERE u.id = $idUsuarios");
     }
 }
